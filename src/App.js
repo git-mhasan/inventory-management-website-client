806e8db70fed55382ebe39a25b9763e0f19e5627
git-mhasan <mahadi2020dev@gmail.com>
2022-05-01 08:07:39 +0600

    404 page designed.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from './components/Shared/Footer/Footer';
 import Inventories from './components/Pages/Inventories/Inventories';
 import SingleItem from './components/Pages/SingleItem/SingleItem';
 import Login from './components/Pages/Login/Login';
+import NotFound from './components/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/inventory/:id" element={<SingleItem />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
       <Footer></Footer>
     </div>
--- src/components/Shared/NotFound/NotFound.js
+++ src/components/Shared/NotFound/NotFound.js
@@ -3,8 +3,9 @@ import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>404: not found</h1>
+        <div className="four-oh-four-div">
+            <div className='four-oh-four'>404</div>
+            <button className='button'>Go to Homepage</button>
         </div>
     );
 };
