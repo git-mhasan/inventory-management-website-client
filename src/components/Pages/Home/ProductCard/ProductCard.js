47f1b84d4ec65c2ca0f1092325ee4e77d3138aab
git-mhasan <mahadi2020dev@gmail.com>
2022-05-02 17:14:03 +0600

    Homepage 6 inventory item ok

--- src/components/Pages/Home/ProductCard/ProductCard.js
+++ src/components/Pages/Home/ProductCard/ProductCard.js
@@ -7,6 +7,11 @@ const ProductCard = ({ product }) => {
         <div className='product-card'>
             <img src={img} alt="" />
             <h4>{name.toUpperCase()}</h4>
+            <p className='product-card-desc'>{desc?.length > 65 ? desc?.slice(0, 65) + "..." : desc}</p>
+            <p>Supplier: {supplier}</p>
+            <p>Price: Tk. <b>{price}</b></p>
+            <p>In stock: {quantity} pcs</p>
+            <button className='button'>Manage Stock</button>
         </div>
     );
 };
